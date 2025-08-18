9de4dd384dfef4da049135c6304b51ff0fc86f78
saritapiedrag <saritapiedrag@gmail.com>
2025-08-17 22:58:02 -0500

    Cambio en db.js para consultas ilimitadas

--- config/db.js
+++ config/db.js
@@ -6,7 +6,9 @@ const db = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 db.connect(err => {
